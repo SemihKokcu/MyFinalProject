2345f6b32432c884e8107a878ada6768b64a2acf
SemihKokcu <semihkkc1@gmail.com>
2022-09-27 12:48:11 +0300

    added database integration

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,16 +1,22 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID 
+    /*
+     *Open Closed Principle => yeni bir özellik geldiğinde mevcutta ki hiç bir kod değişmemelidir.
+     * 
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetAllByUnitPrice(50,100))
             {
                 Console.WriteLine(product.ProductName);
             }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // generic constraint
+    //class : referans tip
+    // IEntity: IEntity olabilir ya da onu implemente eden bir nesne olabilir
+    // new() : newlenebilr olmalı bu durumda IEntiy olmaz ve işe yarar
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter); // tek veri gelicek filter zorunlu
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
