6847be735aeae08caca592383928e07008afcd37
SemihKokcu <semihkkc1@gmail.com>
2022-10-06 01:10:01 +0300

    jwt adding before

---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SigningCredentialsHelper
+    {//sisteme girmek için anahtar
+        public static SigningCredentials CreateSigningCredentials(SecurityKey security)
+        {
+            //anahtar olarak security keyi al, şifreleme olarak HmacSha512signature
+            return new SigningCredentials(security,SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
+
+//aspnet için girişlerde ne kullanması gerektiğini söyledik, json web token için
---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{//araç
+    public class HashingHelper
+    {//out => dışarıya verilecek depğer
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;  //saltta hmac in key ini kullanıyoruz
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));  // byte karşılığını hasliyor
+
+            }
+        }
+        // veri tabdan gelen hashi ve saltı alıyoruz
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {                                                               //key ver
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));  //salta göre yapıyor
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i]!=passwordHash[i])
+                    {
+                        return false;
+                    }
+                    
+                }
+            }
+
+            return true;
+        }
+    }
+}
