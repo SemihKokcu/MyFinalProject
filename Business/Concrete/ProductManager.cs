e694f87539e61bcead2a1403cde66f25a7b0e953
SemihKokcu <semihkkc1@gmail.com>
2022-10-24 04:37:34 +0300

    caching added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -49,15 +49,10 @@ namespace Business.Concrete
             }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
-
-           
            
             
-
         }
-
-
-
+        //[CacheAspect] //key,value
         public IDataResult<List<Product>> GetAll()
         {
             //if (DateTime.Now.Hour == 21)
